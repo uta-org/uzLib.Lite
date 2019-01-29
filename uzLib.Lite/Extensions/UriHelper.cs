b282e3a0daa4a857d9e094a03f34e561cca0b443
Ikillnukes <ikillnukes1@gmail.com>
2019-01-29 18:15:11 +0100

    Added UriHelper

---
+++ uzLib.Lite/Extensions/UriHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace uzLib.Lite.Extensions
+{
+    public static class UriHelper
+    {
+        public static bool CheckURLValid(this string source)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(source, UriKind.Absolute, out uriResult);
+        }
+    }
+}
