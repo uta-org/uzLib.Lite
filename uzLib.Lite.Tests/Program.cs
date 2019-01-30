8bf2863299e44c0c9082f92dde6f625fc6978919
Ikillnukes <ikillnukes1@gmail.com>
2019-01-30 21:40:00 +0100

    Improved ConsoleOutput (added Insert, Arrows, Delete)...

---
+++ uzLib.Lite.Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzLib.Lite.Tests
+{
+    using Core;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Write test string: ");
+            var test = ConsoleOutput.ReadLineOrKey();
+
+            if (test.IsKey())
+                Console.WriteLine(test.KeyName);
+            else
+                Console.WriteLine($"Output string: {test.OutputString}");
+            Console.Read();
+        }
+    }
+}
