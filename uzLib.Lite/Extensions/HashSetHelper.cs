437acaa676635564b80c3a7fa36c04b165cbc822
Ikillnukes <ikillnukes1@gmail.com>
2019-01-31 04:47:06 +0100

    HashSet extensions impl

---
+++ uzLib.Lite/Extensions/HashSetHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace uzLib.Lite.Extensions
+{
+    public static class HashSetHelper
+    {
+        public static void RemoveAt<T>(this HashSet<T> hashset, int index)
+        {
+            hashset.Remove(hashset.ElementAt(index));
+        }
+
+        public static int IndexOf<T>(this HashSet<T> hashset, T item)
+        {
+            for (int i = 0; i < hashset.Count; ++i)
+                if (hashset.ElementAt(i).Equals(item))
+                    return i;
+
+            return -1;
+        }
+    }
+}
