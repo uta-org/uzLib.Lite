6a1ed8fefd1120e79bf971ce3ce7e2ea3953bff1
Ikillnukes <ikillnukes1@gmail.com>
2019-02-12 22:35:25 +0100

    Re-added GitShell && ProcessHelper

---
+++ uzLib.Lite/Extensions/GitHelper.cs
@@ -0,0 +1,22 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzLib.Lite.Extensions
+{
+    public static class GitHelper
+    {
+        public static string GetRemoteUrl(string repoPath)
+        {
+            using (var repo = new Repository(repoPath))
+            {
+                var config = repo.Config;
+            }
+
+            return "";
+        }
+    }
+}
---
+++ uzLib.Lite/Shells/GitShell.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using uzLib.Lite.Extensions;
+
+namespace uzLib.Lite.Shells
+{
+    public class GitShell
+    {
+        public ProcessStartInfo CurrentInfo { get; private set; }
+
+        public GitShell()
+        {
+            CurrentInfo = new ProcessStartInfo("git");
+
+            CurrentInfo.UseShellExecute = false;
+            CurrentInfo.RedirectStandardInput = true;
+            CurrentInfo.RedirectStandardOutput = true;
+            CurrentInfo.RedirectStandardError = true;
+        }
+
+        public Task<int> SendCommand(string command)
+        {
+            CurrentInfo.Arguments = command;
+
+            return CurrentInfo.RunProcessAsync();
+        }
+
+        public async Task<string> ReadCommand(string command)
+        {
+            CurrentInfo.Arguments = command;
+
+            using (Process process = new Process())
+            {
+                await CurrentInfo.RunProcessAsync(process);
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+    }
+}
