b66234d86080458ea9eb2589a427e1a178b65ac8
Ikillnukes <ikillnukes1@gmail.com>
2019-02-15 19:11:02 +0100

    ASM extensions added

---
+++ uzLib.Lite/Extensions/ReflectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace uzLib.Lite.Extensions
+{
+    public static class ReflectionHelper
+    {
+        public static void InvokeStaticMethod(this Assembly asm, string className, string methodName, params object[] parameters)
+        {
+            asm.GetType(className).GetMethod(methodName).Invoke(null, parameters);
+
+            //foreach (var _a in AppDomain.CurrentDomain.GetAssemblies())
+            //{
+            //    foreach (var _t in _a.GetTypes())
+            //    {
+            //        try
+            //        {
+            //            if ((_t.Namespace == namespaceName) && _t.IsClass) _t.GetMethod(methodName, (BindingFlags.Static | BindingFlags.Public))?.Invoke(null, parameters);
+            //        }
+            //        catch { }
+            //    }
+            //}
+        }
+    }
+}
