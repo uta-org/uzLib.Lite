7d1d05f23497779d93bef2579415b823d1c034ad
Ikillnukes <ikillnukes1@gmail.com>
2019-02-19 15:54:01 +0100

    Removed unused NuGet packages + added MSBuild support

---
+++ uzLib.Lite/Extensions/CompilerHelper.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//using System.Drawing;
+
+//using Console = Colorful.Console;
+
+namespace uzLib.Lite.Extensions
+{
+    using Core;
+
+    public static class CompilerHelper
+    {
+        public static bool Compile(string solutionPath, string outputDir, out string outString)
+        {
+            outString = "";
+
+            bool isException = false;
+            ProjectCollection pc = new ProjectCollection();
+
+            // THERE ARE A LOT OF PROPERTIES HERE, THESE MAP TO THE MSBUILD CLI PROPERTIES
+            Dictionary<string, string> globalProperty = new Dictionary<string, string>();
+            globalProperty.Add("OutputPath", outputDir);
+
+            var logger = new MsBuildMemoryLogger();
+
+            BuildParameters bp = new BuildParameters(pc);
+            bp.Loggers = new[] { logger };
+
+            BuildRequestData buildRequest = new BuildRequestData(solutionPath, globalProperty, ToolLocationHelper.CurrentToolsVersion, new string[] { "Build" }, null);
+
+            // THIS IS WHERE THE MAGIC HAPPENS - IN PROCESS MSBUILD
+            try
+            {
+                BuildResult buildResult = BuildManager.DefaultBuildManager.Build(bp, buildRequest);
+                // A SIMPLE WAY TO CHECK THE RESULT
+                if (buildResult.OverallResult == BuildResultCode.Success)
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                isException = true;
+                outString = ex.ToString();
+            }
+            finally
+            {
+                pc.UnregisterAllLoggers();
+                logger.Shutdown();
+            }
+
+            if (isException)
+                return false;
+
+            outString = logger.GetLog();
+            return logger.HasErrors;
+        }
+
+        //private static string GetCurrentToolsVersion()
+        //{
+        //    var query = new SetupConfiguration();
+
+        //    var query2 = (ISetupConfiguration2)query;
+
+        //    var e = query2.EnumAllInstances();
+
+        //    var helper = (ISetupHelper)query;
+
+        //    int fetched;
+
+        //    var instances = new ISetupInstance[1];
+
+        //    do
+        //    {
+        //        e.Next(1, instances, out fetched);
+        //        if (fetched > 0)
+        //        {
+        //            var instance = instances[0];
+
+        //            var instance2 = (ISetupInstance2)instance;
+
+        //            var state = instance2.GetState();
+
+        //            // Skip non-complete instance, I guess?
+        //            // Skip non-local instance, I guess?
+        //            // Skip unregistered products?
+        //            if (state != InstanceState.Complete
+        //                || (state & InstanceState.Local) != InstanceState.Local
+        //                || (state & InstanceState.Registered) != InstanceState.Registered)
+        //            {
+        //                continue;
+        //            }
+
+        //            var msBuildComponent =
+        //                instance2.GetPackages()
+        //                    .FirstOrDefault(
+        //                        p =>
+        //                            p.GetId()
+        //                                .Equals("Microsoft.Component.MSBuild",
+        //                                    StringComparison.InvariantCultureIgnoreCase));
+
+        //            if (msBuildComponent == null)
+        //            {
+        //                continue;
+        //            }
+
+        //            var instanceRootDirectory = instance2.GetInstallationPath();
+
+        //            var msbuildPathInInstance = Path.Combine(instanceRootDirectory, "MSBuild", msBuildVersion, "Bin", "msbuild.exe");
+
+        //            if (File.Exists(msbuildPathInInstance))
+        //            {
+        //                return msbuildPathInInstance;
+        //            }
+        //        }
+        //    } while (fetched > 0);
+        //}
+    }
+}
--- uzLib.Lite/Extensions/RoslynHelper.cs
+++ uzLib.Lite/Extensions/RoslynHelper.cs
@@ -1,226 +1,241 @@
-﻿using Microsoft.Build.Execution;
-using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Emit;
-using Microsoft.CodeAnalysis.FindSymbols;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
+﻿using Microsoft.Build.Evaluation;
+
+//using Microsoft.Build.Execution;
+//using Microsoft.Build.Locator;
+//using Microsoft.CodeAnalysis;
+//using Microsoft.CodeAnalysis.CSharp;
+//using Microsoft.CodeAnalysis.CSharp.Syntax;
+//using Microsoft.CodeAnalysis.Emit;
+//using Microsoft.CodeAnalysis.FindSymbols;
+//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Globalization;
 using System.Linq;
 
-using Console = Colorful.Console;
+//using Console = Colorful.Console;
 
 namespace uzLib.Lite.Extensions
 {
     public static class RoslynHelper
     {
-        private static int dupedItems = 0,
-                           noDupedItems = 0;
-
-        private static HashSet<string> assemblyNames = new HashSet<string>();
-
-        /*private const string ProjectLiteral = "ResolveProjectReferences",
-                             AssemblyLiteral = "ResolveAssemblyReferences";
-
-        public static VisualStudioInstance GetMSBuildInstance(string msBuildPath = "", bool batchMode = false)
+        public static void AddItem(string projPath, string folderPath, string saveFilePath)
         {
-            // найдём версию MSBuild
-            var visualStudioInstances =
-                MSBuildLocator.QueryVisualStudioInstances().ToArray();
+            // Solved issue thanks to: https://stackoverflow.com/a/44260284/3286975
 
-            var instance =
-                // указан путь, выбираем его
-                !string.IsNullOrEmpty(msBuildPath) ? visualStudioInstances.SingleOrDefault(
-                                                   i => i.MSBuildPath == msBuildPath) :
-                // только одна студия, выбираем её
-                visualStudioInstances.Length == 1 ? visualStudioInstances[0] :
-                // в пакетном режиме? падаем
-                batchMode ? null :
-                // спрашиваем у юзера
-                SelectVisualStudioInstance(visualStudioInstances);
+            Project project = new Project(projPath);
 
-            if (instance == null)
-            {
-                Console.WriteLine("Cannot determine MSBuild path");
-                return null;
-            }
+            string relPath = IOHelper.MakeRelativePath(folderPath, saveFilePath);
+            if (!project.GetItems("Compile").Any(item => item.UnevaluatedInclude == relPath))
+                project.AddItem("Compile", relPath);
 
-            return instance;
+            project.Save();
         }
 
-        private static VisualStudioInstance SelectVisualStudioInstance(
-        VisualStudioInstance[] visualStudioInstances)
-        {
-            Console.WriteLine("Multiple installs of MSBuild detected, please select one:");
-            for (int i = 0; i < visualStudioInstances.Length; i++)
-            {
-                Console.WriteLine($"Instance {i + 1}");
-                Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
-                Console.WriteLine($"    Version: {visualStudioInstances[i].Version}");
-                Console.WriteLine(
-                    $"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
-            }
+        //private static int dupedItems = 0,
+        //                   noDupedItems = 0;
 
-            while (true)
-            {
-                var userResponse = Console.ReadLine();
-                if (int.TryParse(userResponse, out int instanceNumber) &&
-                    instanceNumber > 0 &&
-                    instanceNumber <= visualStudioInstances.Length)
-                {
-                    return visualStudioInstances[instanceNumber - 1];
-                }
-                Console.WriteLine("Input not accepted, try again.");
-            }
-        }*/
+        //private static HashSet<string> assemblyNames = new HashSet<string>();
 
-        public static void StartDebugging()
-        {
-            assemblyNames.Clear();
-        }
+        ///*private const string ProjectLiteral = "ResolveProjectReferences",
+        //                     AssemblyLiteral = "ResolveAssemblyReferences";
 
-        public static CSharpCompilation AddAllMissingReferences(this CSharpCompilation compilation, Solution solution, Project project)
-        {
-            if (assemblyNames == null)
-                assemblyNames = new HashSet<string>();
+        //public static VisualStudioInstance GetMSBuildInstance(string msBuildPath = "", bool batchMode = false)
+        //{
+        //    // найдём версию MSBuild
+        //    var visualStudioInstances =
+        //        MSBuildLocator.QueryVisualStudioInstances().ToArray();
 
-            foreach (var document in project.Documents)
-            {
-                var model = document.GetSemanticModelAsync().Result;
-                var root = document.GetSyntaxRootAsync().Result;
+        //    var instance =
+        //        // указан путь, выбираем его
+        //        !string.IsNullOrEmpty(msBuildPath) ? visualStudioInstances.SingleOrDefault(
+        //                                           i => i.MSBuildPath == msBuildPath) :
+        //        // только одна студия, выбираем её
+        //        visualStudioInstances.Length == 1 ? visualStudioInstances[0] :
+        //        // в пакетном режиме? падаем
+        //        batchMode ? null :
+        //        // спрашиваем у юзера
+        //        SelectVisualStudioInstance(visualStudioInstances);
 
-                root.DescendantNodesAndSelf().OfType<UsingDirectiveSyntax>().ForEach(@using =>
-                {
-                    // Thanks to: https://stackoverflow.com/a/13688863/3286975
-                    NameSyntax ns = @using.Name;
+        //    if (instance == null)
+        //    {
+        //        Console.WriteLine("Cannot determine MSBuild path");
+        //        return null;
+        //    }
 
-                    if (assemblyNames.Add(ns.GetText().ToString()))
-                        ++noDupedItems;
-                    else
-                        ++dupedItems;
-                });
+        //    return instance;
+        //}
 
-                // WIP: Remove relative using directives
-                // +++: Add system references with the typeof(object) trick
-                // +++: Detect NuGet packages (they can be obtained by reading the csproj file (with the other class))
-                // +++: Detect ProjectReferences (and compile it if needed (recursivity here))
+        //private static VisualStudioInstance SelectVisualStudioInstance(
+        //VisualStudioInstance[] visualStudioInstances)
+        //{
+        //    Console.WriteLine("Multiple installs of MSBuild detected, please select one:");
+        //    for (int i = 0; i < visualStudioInstances.Length; i++)
+        //    {
+        //        Console.WriteLine($"Instance {i + 1}");
+        //        Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
+        //        Console.WriteLine($"    Version: {visualStudioInstances[i].Version}");
+        //        Console.WriteLine(
+        //            $"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
+        //    }
 
-                //var symbols = compilation.GetAllSymbols(root);
+        //    while (true)
+        //    {
+        //        var userResponse = Console.ReadLine();
+        //        if (int.TryParse(userResponse, out int instanceNumber) &&
+        //            instanceNumber > 0 &&
+        //            instanceNumber <= visualStudioInstances.Length)
+        //        {
+        //            return visualStudioInstances[instanceNumber - 1];
+        //        }
+        //        Console.WriteLine("Input not accepted, try again.");
+        //    }
+        //}*/
 
-                //foreach (var symbol in symbols)
-                //    foreach (var item in SymbolFinder.FindReferencesAsync(symbol, solution).Result)
-                //        foreach (var location in item.Locations)
-                //        {
-                //            //Console.WriteLine($"Found assembly {location.Document.Project.AssemblyName}!", Color.Green);
+        //public static void StartDebugging()
+        //{
+        //    assemblyNames.Clear();
+        //}
 
-                //            if (assemblyNames.Add(location.Document.Project.AssemblyName))
-                //                ++noDupedItems;
-                //            else
-                //                ++dupedItems;
-                //        }
-            }
+        //public static CSharpCompilation AddAllMissingReferences(this CSharpCompilation compilation, Solution solution, Project project)
+        //{
+        //    if (assemblyNames == null)
+        //        assemblyNames = new HashSet<string>();
 
-            //SymbolFinder.FindReferencesAsync(null, null).Result.ForEach(r => compilation.AddReferences());
+        //    foreach (var document in project.Documents)
+        //    {
+        //        var model = document.GetSemanticModelAsync().Result;
+        //        var root = document.GetSyntaxRootAsync().Result;
 
-            // Thanks to: https://stackoverflow.com/a/32770484/3286975
-            //return compilation.AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+        //        root.DescendantNodesAndSelf().OfType<UsingDirectiveSyntax>().ForEach(@using =>
+        //        {
+        //            // Thanks to: https://stackoverflow.com/a/13688863/3286975
+        //            NameSyntax ns = @using.Name;
 
-            //return compilation.AddReferences(GetReferences(project.FilePath).Select(path => MetadataReference.CreateFromFile(path)));
+        //            if (assemblyNames.Add(ns.GetText().ToString()))
+        //                ++noDupedItems;
+        //            else
+        //                ++dupedItems;
+        //        });
 
-            return null;
-        }
+        //        // WIP: Remove relative using directives
+        //        // +++: Add system references with the typeof(object) trick
+        //        // +++: Detect NuGet packages (they can be obtained by reading the csproj file (with the other class))
+        //        // +++: Detect ProjectReferences (and compile it if needed (recursivity here))
 
-        public static void EndDebugging()
-        {
-            Console.WriteLine();
+        //        //var symbols = compilation.GetAllSymbols(root);
 
-            Console.WriteLine($"There was found a total of {noDupedItems} references (not duped).");
-            Console.WriteLine($"Duped item count: {dupedItems} (Total: {dupedItems + noDupedItems})");
+        //        //foreach (var symbol in symbols)
+        //        //    foreach (var item in SymbolFinder.FindReferencesAsync(symbol, solution).Result)
+        //        //        foreach (var location in item.Locations)
+        //        //        {
+        //        //            //Console.WriteLine($"Found assembly {location.Document.Project.AssemblyName}!", Color.Green);
 
-            Console.WriteLine();
+        //        //            if (assemblyNames.Add(location.Document.Project.AssemblyName))
+        //        //                ++noDupedItems;
+        //        //            else
+        //        //                ++dupedItems;
+        //        //        }
+        //    }
 
-            assemblyNames.ForEach(name => Console.WriteLine($"Found assembly {name}!", Color.Green));
+        //    //SymbolFinder.FindReferencesAsync(null, null).Result.ForEach(r => compilation.AddReferences());
 
-            Console.WriteLine();
-        }
+        //    // Thanks to: https://stackoverflow.com/a/32770484/3286975
+        //    //return compilation.AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
 
-        public static IEnumerable<ISymbol> GetAllSymbols(this CSharpCompilation compilation, SyntaxNode root)
-        {
-            var noDuplicates = new HashSet<ISymbol>();
+        //    //return compilation.AddReferences(GetReferences(project.FilePath).Select(path => MetadataReference.CreateFromFile(path)));
 
-            var model = compilation.GetSemanticModel(root.SyntaxTree);
+        //    return null;
+        //}
 
-            foreach (var node in root.DescendantNodesAndSelf())
-            {
-                switch (node.Kind())
-                {
-                    case SyntaxKind.ExpressionStatement:
-                    case SyntaxKind.InvocationExpression:
-                        break;
+        //public static void EndDebugging()
+        //{
+        //    Console.WriteLine();
 
-                    default:
-                        ISymbol symbol = model.GetSymbolInfo(node).Symbol;
+        //    Console.WriteLine($"There was found a total of {noDupedItems} references (not duped).");
+        //    Console.WriteLine($"Duped item count: {dupedItems} (Total: {dupedItems + noDupedItems})");
 
-                        if (symbol != null)
-                        {
-                            if (noDuplicates.Add(symbol))
-                                yield return symbol;
-                        }
-                        break;
-                }
-            }
-        }
+        //    Console.WriteLine();
 
-        // Thanks to: https://csharp.hotexamples.com/examples/Microsoft.CodeAnalysis.Emit/EmitResult/-/php-emitresult-class-examples.html
-        public static string VerifyCompilationResults(this EmitResult compilationResult)
-        {
-            if (compilationResult.Success)
-                return string.Empty;
+        //    assemblyNames.ForEach(name => Console.WriteLine($"Found assembly {name}!", Color.Green));
 
-            string errorsString = string.Join(
-                   Environment.NewLine,
-                   compilationResult.Diagnostics
-                       .Where(diagnostic => diagnostic.IsWarningAsError ||
-                                            diagnostic.Severity == DiagnosticSeverity.Error)
-                       .Select(diagnostic => $"{diagnostic.Id}: {diagnostic.GetMessage(CultureInfo.CurrentUICulture)}"));
+        //    Console.WriteLine();
+        //}
 
-            return errorsString;
-        }
+        //public static IEnumerable<ISymbol> GetAllSymbols(this CSharpCompilation compilation, SyntaxNode root)
+        //{
+        //    var noDuplicates = new HashSet<ISymbol>();
 
-        // Thanks to: https://stackoverflow.com/a/43105401/3286975
-        /*private static IEnumerable<string> GetReferences(string projectFileName)
-        {
-            var projectInstance = new ProjectInstance(projectFileName);
-            var result = BuildManager.DefaultBuildManager.Build(
-                new BuildParameters(),
-                new BuildRequestData(projectInstance, new[]
-                {
-                    ProjectLiteral,
-                    AssemblyLiteral
-                }));
+        //    var model = compilation.GetSemanticModel(root.SyntaxTree);
 
-            IEnumerable<string> GetResultItems(string targetName)
-            {
-                if (!result.ResultsByTarget.ContainsKey(targetName))
-                    return null;
+        //    foreach (var node in root.DescendantNodesAndSelf())
+        //    {
+        //        switch (node.Kind())
+        //        {
+        //            case SyntaxKind.ExpressionStatement:
+        //            case SyntaxKind.InvocationExpression:
+        //                break;
 
-                var buildResult = result.ResultsByTarget[targetName];
-                var buildResultItems = buildResult.Items;
+        //            default:
+        //                ISymbol symbol = model.GetSymbolInfo(node).Symbol;
 
-                return buildResultItems.Select(item => item.ItemSpec);
-            }
+        //                if (symbol != null)
+        //                {
+        //                    if (noDuplicates.Add(symbol))
+        //                        yield return symbol;
+        //                }
+        //                break;
+        //        }
+        //    }
+        //}
 
-            var projectResults = GetResultItems(ProjectLiteral);
+        //// Thanks to: https://csharp.hotexamples.com/examples/Microsoft.CodeAnalysis.Emit/EmitResult/-/php-emitresult-class-examples.html
+        //public static string VerifyCompilationResults(this EmitResult compilationResult)
+        //{
+        //    if (compilationResult.Success)
+        //        return string.Empty;
 
-            //if (projectResults.IsNullOrEmpty())
-            //    throw new Exception($"There was a problem calling '{ProjectLiteral}'.");
+        //    string errorsString = string.Join(
+        //           Environment.NewLine,
+        //           compilationResult.Diagnostics
+        //               .Where(diagnostic => diagnostic.IsWarningAsError ||
+        //                                    diagnostic.Severity == DiagnosticSeverity.Error)
+        //               .Select(diagnostic => $"{diagnostic.Id}: {diagnostic.GetMessage(CultureInfo.CurrentUICulture)}"));
 
-            var assemblyResults = GetResultItems(AssemblyLiteral);
+        //    return errorsString;
+        //}
 
-            return assemblyResults.IsNullOrEmpty() ? projectResults : projectResults.Concat(assemblyResults);
-        }*/
+        //// Thanks to: https://stackoverflow.com/a/43105401/3286975
+        ///*private static IEnumerable<string> GetReferences(string projectFileName)
+        //{
+        //    var projectInstance = new ProjectInstance(projectFileName);
+        //    var result = BuildManager.DefaultBuildManager.Build(
+        //        new BuildParameters(),
+        //        new BuildRequestData(projectInstance, new[]
+        //        {
+        //            ProjectLiteral,
+        //            AssemblyLiteral
+        //        }));
+
+        //    IEnumerable<string> GetResultItems(string targetName)
+        //    {
+        //        if (!result.ResultsByTarget.ContainsKey(targetName))
+        //            return null;
+
+        //        var buildResult = result.ResultsByTarget[targetName];
+        //        var buildResultItems = buildResult.Items;
+
+        //        return buildResultItems.Select(item => item.ItemSpec);
+        //    }
+
+        //    var projectResults = GetResultItems(ProjectLiteral);
+
+        //    //if (projectResults.IsNullOrEmpty())
+        //    //    throw new Exception($"There was a problem calling '{ProjectLiteral}'.");
+
+        //    var assemblyResults = GetResultItems(AssemblyLiteral);
+
+        //    return assemblyResults.IsNullOrEmpty() ? projectResults : projectResults.Concat(assemblyResults);
+        //}*/
     }
 }
