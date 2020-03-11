ad5db7908a949ecf361df919fcb39442da29a6a8
z3nth10n <z3nth10n@gmail.com>
2020-03-12 00:15:23 +0100

    Moved HandledMonoBehaviour file

---
+++ uzLib.Lite.ExternalCode/Unity/Utils/HandledMonoBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using uzLib.Lite.ExternalCode.Core;
+using uzLib.Lite.ExternalCode.Unity.Utils;
+
+namespace uzLib.Lite.ExternalCode.Utils
+{
+    //[AutoInstantiate]
+    public abstract class HandledMonoBehaviour : MonoSingleton<HandledMonoBehaviour>
+    {
+        public abstract void OnQuit();
+
+        public void OnApplicationQuit()
+        {
+            OnQuit();
+        }
+    }
+}
--- uzLib.Lite.ExternalCode/Unity/Utils/MonoUtils.cs
+++ uzLib.Lite.ExternalCode/Unity/Utils/MonoUtils.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
-using uzLib.Lite.ExternalCode.Core;
-using uzLib.Lite.ExternalCode.Unity.Utils;
 using Object = UnityEngine.Object;
 
 namespace uzLib.Lite.ExternalCode.Utils
@@ -12,17 +10,6 @@ namespace uzLib.Lite.ExternalCode.Utils
         public static MonoBehaviour FindExecutingInEditModeMonoBehaviour()
         {
             return Object.FindObjectsOfType<MonoBehaviour>().FirstOrDefault(c => Attribute.GetCustomAttribute(c.GetType(), typeof(ExecuteInEditMode)) != null);
-        }
-    }
-
-    [AutoInstantiate]
-    public abstract class HandledMonoBehaviour : MonoSingleton<HandledMonoBehaviour>
-    {
-        public abstract void OnQuit();
-
-        public void OnApplicationQuit()
-        {
-            OnQuit();
         }
     }
 }
