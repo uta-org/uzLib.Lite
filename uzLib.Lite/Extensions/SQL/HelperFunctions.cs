e6d3b9507246595119ab09c030c124ca0a1a9562
z3nth10n <z3nth10n@gmail.com>
2020-07-17 20:38:56 +0200

    Add SQL helpers and compile

---
+++ uzLib.Lite/Extensions/SQL/HelperFunctions.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CCC.ORM.Helpers
+{
+    public static class HelperFunctions
+    {
+        public static bool GetResolvedConnecionIpAddress(string serverNameOrUrl, out string resolvedIpAddress)
+        {
+            var isResolved = false;
+            IPAddress resolvIp = null;
+            try
+            {
+                if (!IPAddress.TryParse(serverNameOrUrl, out resolvIp))
+                {
+                    var hostEntry = Dns.GetHostEntry(serverNameOrUrl);
+
+                    if (hostEntry != null && hostEntry.AddressList != null
+                        && hostEntry.AddressList.Length > 0)
+                    {
+                        if (hostEntry.AddressList.Length == 1)
+                        {
+                            resolvIp = hostEntry.AddressList[0];
+                            isResolved = true;
+                        }
+                        else
+                        {
+                            foreach (var var in hostEntry.AddressList.Where(var => var.AddressFamily == AddressFamily.InterNetwork))
+                            {
+                                resolvIp = var;
+                                isResolved = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    isResolved = true;
+                }
+            }
+            catch (Exception)
+            {
+                isResolved = false;
+                resolvIp = null;
+            }
+            finally
+            {
+                if (resolvIp != null) resolvedIpAddress = resolvIp.ToString();
+            }
+
+            resolvedIpAddress = null;
+            return isResolved;
+        }
+
+        public static string SerializeObject<T>(T source)
+        {
+            var serializer = new XmlSerializer(typeof (T));
+
+            using (var sw = new StringWriter())
+            using (var writer = new XmlTextWriter(sw))
+            {
+                serializer.Serialize(writer, source);
+                return sw.ToString();
+            }
+        }
+
+        public static T DeSerializeObject<T>(string xml)
+        {
+            using (var sr = new StringReader(xml))
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                return (T) serializer.Deserialize(sr);
+            }
+        }
+
+        public static object ReturnZeroIfNull(this object value)
+        {
+            if (value == DBNull.Value)
+                return 0;
+            if (value == null)
+                return 0;
+            return value;
+        }
+
+        public static object ReturnEmptyIfNull(this object value)
+        {
+            if (value == DBNull.Value)
+                return string.Empty;
+            if (value == null)
+                return string.Empty;
+            return value;
+        }
+
+        public static object ReturnFalseIfNull(this object value)
+        {
+            if (value == DBNull.Value)
+                return false;
+            if (value == null)
+                return false;
+            return value;
+        }
+
+        public static object ReturnDateTimeMinIfNull(this object value)
+        {
+            if (value == DBNull.Value)
+                return DateTime.MinValue;
+            if (value == null)
+                return DateTime.MinValue;
+            return value;
+        }
+
+        public static object ReturnNullIfDbNull(this object value)
+        {
+            if (value == DBNull.Value)
+                return '\0';
+            if (value == null)
+                return '\0';
+            return value;
+        }
+
+        //This function formats the display-name of a user,
+        //and removes unnecessary extra information.
+        public static string FormatUserDisplayName(string displayName = null, string defaultValue = "tBill Users",
+            bool returnNameIfExists = false, bool returnAddressPartIfExists = false)
+        {
+            //Get the first part of the Users's Display Name if s/he has a name like this: "firstname lastname (extra text)"
+            //removes the "(extra text)" part
+            if (!string.IsNullOrEmpty(displayName))
+            {
+                if (returnNameIfExists)
+                    return Regex.Replace(displayName, @"\ \(\w{1,}\)", "");
+                return (displayName.Split(' '))[0];
+            }
+            if (returnAddressPartIfExists)
+            {
+                var emailParts = defaultValue.Split('@');
+                return emailParts[0];
+            }
+            return defaultValue;
+        }
+
+        public static string FormatUserTelephoneNumber(this string telephoneNumber)
+        {
+            var result = string.Empty;
+
+            if (!string.IsNullOrEmpty(telephoneNumber))
+            {
+                //result = telephoneNumber.ToLower().Trim().Trim('+').Replace("tel:", "");
+                result = telephoneNumber.ToLower().Trim().Replace("tel:", "");
+
+                if (result.Contains(";"))
+                {
+                    if (!result.ToLower().Contains(";ext="))
+                        result = result.Split(';')[0];
+                }
+            }
+
+            return result;
+        }
+
+        public static bool IsValidEmail(this string emailAddress)
+        {
+            const string pattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+
+            return Regex.IsMatch(emailAddress, pattern);
+        }
+
+        /// <summary>
+        /// Convert DateTime to string
+        /// </summary>
+        /// <param name="datetTime"></param>
+        /// <param name="excludeHoursAndMinutes">if true it will execlude time from datetime string. Default is false</param>
+        /// <returns></returns>
+        public static string ConvertDate(this DateTime datetTime, bool excludeHoursAndMinutes = false)
+        {
+            if (datetTime != DateTime.MinValue)
+            {
+                if (excludeHoursAndMinutes)
+                    return datetTime.ToString("yyyy-MM-dd");
+                return datetTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            }
+            return null;
+        }
+
+        [SuppressMessage("ReSharper", "PossibleLossOfFraction")]
+        public static string ConvertSecondsToReadable(this int secondsParam)
+        {
+            var hours = Convert.ToInt32(Math.Floor((double) (secondsParam/3600)));
+            var minutes = Convert.ToInt32(Math.Floor((double) (secondsParam - (hours*3600))/60));
+            var seconds = secondsParam - (hours*3600) - (minutes*60);
+
+            var hoursStr = hours.ToString();
+            var minsStr = minutes.ToString();
+            var secsStr = seconds.ToString();
+
+            if (hours < 10)
+            {
+                hoursStr = "0" + hoursStr;
+            }
+
+            if (minutes < 10)
+            {
+                minsStr = "0" + minsStr;
+            }
+            if (seconds < 10)
+            {
+                secsStr = "0" + secsStr;
+            }
+
+            return hoursStr + ':' + minsStr + ':' + secsStr;
+        }
+
+        [SuppressMessage("ReSharper", "PossibleLossOfFraction")]
+        public static string ConvertSecondsToReadable(this long secondsParam)
+        {
+            var hours = Convert.ToInt32(Math.Floor((double) (secondsParam/3600)));
+            var minutes = Convert.ToInt32(Math.Floor((double) (secondsParam - (hours*3600))/60));
+            var seconds = Convert.ToInt32(secondsParam - (hours*3600) - (minutes*60));
+
+            var hoursStr = hours.ToString();
+            var minsStr = minutes.ToString();
+            var secsStr = seconds.ToString();
+
+            if (hours < 10)
+            {
+                hoursStr = "0" + hoursStr;
+            }
+
+            if (minutes < 10)
+            {
+                minsStr = "0" + minsStr;
+            }
+            if (seconds < 10)
+            {
+                secsStr = "0" + secsStr;
+            }
+
+            return hoursStr + ':' + minsStr + ':' + secsStr;
+        }
+    }
+}
---
+++ uzLib.Lite/Extensions/SQL/MatchingExtention.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CCC.ORM.Helpers
+{
+    public static class MatchingExtention
+    {
+        public static bool In<T>(this T x, params T[] values)
+        {
+            return values.Contains(x);
+        }
+    }
+}
