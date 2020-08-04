e889d9185dfe65c6f6b1fcd246674bbc28ffff11
z3nth10n <z3nth10n@gmail.com>
2020-08-04 15:26:07 +0200

    Add some extensions: Net, NVC, Stream

---
+++ uzLib.Lite/Extensions/NVCHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace uzLib.Lite.Extensions
+{
+    public static class NVCHelper
+    {
+        public static IDictionary<string, string> ToDictionary(
+            this NameValueCollection source)
+        {
+            return source.AllKeys.ToDictionary(k => k, k => source[k]);
+        }
+
+        public static string ToJson(this NameValueCollection source)
+        {
+            return JsonConvert.SerializeObject(source.ToDictionary());
+        }
+    }
+}
--- uzLib.Lite/Extensions/NetHelper.cs
+++ uzLib.Lite/Extensions/NetHelper.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using uzLib.Lite.Extensions;
 
 namespace UnityEngine.Extensions
 {
@@ -144,6 +149,34 @@ namespace UnityEngine.Extensions
         public static bool IsUrl(this string p)
         {
             return !new Uri(p).IsFile;
+        }
+
+        public static async Task<long> DownloadAsync(this HttpClient client, string requestUri, Stream destination, IProgress<float> progress = null, CancellationToken cancellationToken = default)
+        {
+            // Get the http headers first to examine the content length
+            using (var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+                var contentLength = response.Content.Headers.ContentLength;
+
+                using (var download = await response.Content.ReadAsStreamAsync())
+                {
+                    // Ignore progress reporting when no progress reporter was
+                    // passed or when the content length is unknown
+                    if (progress == null || !contentLength.HasValue)
+                    {
+                        await download.CopyToAsync(destination);
+                        return destination.Length;
+                    }
+
+                    // Convert absolute progress (bytes downloaded) into relative progress (0% - 100%)
+                    var relativeProgress = new Progress<long>(totalBytes => progress.Report((float)totalBytes / contentLength.Value));
+                    // Use extension method to report progress while downloading
+                    await download.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
+                    progress.Report(1);
+
+                    return contentLength.Value;
+                }
+            }
         }
     }
 }
