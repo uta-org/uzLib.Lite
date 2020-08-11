9fcaedf746c0f5c08a0acde84f0d7fd75370f2da
z3nth10n <z3nth10n@gmail.com>
2020-08-12 01:38:55 +0200

    Add ConcurrentHashSet

--- uzLib.Lite.ExternalCode/Unity/Extensions/MathHelper.cs
+++ uzLib.Lite.ExternalCode/Unity/Extensions/MathHelper.cs
@@ -27,6 +27,29 @@ namespace uzLib.Lite.ExternalCode.Unity.Extensions
             return value.ToString("#,0");
         }
 
+        public static string ToSI(this long l, string format = null)
+        {
+            return ((double)l).ToSI(format);
+        }
+
+        public static string ToSI(this double d, string format = null)
+        {
+            var incPrefixes = new[] { 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y' };
+            var decPrefixes = new[] { 'm', '\u03bc', 'n', 'p', 'f', 'a', 'z', 'y' };
+
+            var degree = (int)Math.Floor(Math.Log10(Math.Abs(d)) / 3);
+            var scaled = d * Math.Pow(1000, -degree);
+
+            char? prefix = null;
+            switch (Math.Sign(degree))
+            {
+                case 1: prefix = incPrefixes[degree - 1]; break;
+                case -1: prefix = decPrefixes[-degree - 1]; break;
+            }
+
+            return scaled.ToString(format) + prefix;
+        }
+
         /// <summary>
         ///     Deletes the numeric part.
         /// </summary>
